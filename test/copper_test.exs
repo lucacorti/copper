165bd8a75eee18b50bc53aa9a500bf5f4b7123ee
Luca Corti <luca@fantacast.it>
2018-12-01 02:43:58 +0100

    Update ankh, add multiple streams test

--- test/copper_test.exs
+++ test/copper_test.exs
@@ -16,13 +16,26 @@ defmodule CopperTest do
 
   test "get", %{client: client} do
     assert :ok = Client.request(client, %Request{})
-    assert_receive {:ankh, :headers, 1, _headers}, 1_000
-    assert_receive {:ankh, :data, 1, _data, _end_stream}, 1_000
+    assert_receive {:ankh, :headers, _, _headers}, 1_000
+    assert_receive {:ankh, :data, _, _data, _end_stream}, 1_000
   end
 
   test "post", %{client: client} do
     assert :ok = Client.request(client, %Request{method: "POST", data: "data"})
+    assert_receive {:ankh, :headers, _, _headers}, 1_000
+    assert_receive {:ankh, :data, _, _data, _end_stream}, 1_000
+  end
+
+  test "multiple requests", %{client: client} do
+    assert :ok = Client.request(client, %Request{method: "HEAD"})
     assert_receive {:ankh, :headers, 1, _headers}, 1_000
-    assert_receive {:ankh, :data, 1, _data, _end_stream}, 1_000
+
+    assert :ok = Client.request(client, %Request{})
+    assert_receive {:ankh, :headers, 3, _headers}, 1_000
+    assert_receive {:ankh, :data, 3, _data, _end_stream}, 1_000
+
+    assert :ok = Client.request(client, %Request{method: "POST", data: "data"})
+    assert_receive {:ankh, :headers, 5, _headers}, 1_000
+    assert_receive {:ankh, :data, 5, _data, _end_stream}, 1_000
   end
 end
