e02ea9643e3921f5bb041022c2a9ed3cfa5d5549
Luca Corti <luca@fantacast.it>
2018-12-06 19:12:42 +0100

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule Copper.Mixfile do
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:credo, ">= 0.0.0", only: :dev},
       {:dialyxir, ">= 0.0.0", only: :dev},
-      {:ankh, ">= 0.4.5"}
+      {:ankh, ">= 0.4.6"}
     ]
   end
 end
